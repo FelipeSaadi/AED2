a8f2a3f42d00b2d2b8aac35ce7b10991d6c4f8a9
FelipeSaadi <felipesaadisiegert@hotmail.com>
2021-08-04 11:37:47 -0300

    feito o trabalho da calculadora com o relatório

--- Algoritmos de Multiplicação - JS/script.js
+++ Algoritmos de Multiplicação - JS/script.js
@@ -7,6 +7,42 @@ const unidMultiplicador = [6, 7, 8, 9];
 let total = 0;
 
 
+// Adição
+const calcAdicao = function (valor1, valor2) {
+    let parcela1 = numParaArray(valor1);
+    let parcela2 = numParaArray(valor2);
+
+    let contParcela1 = parcela1.length - 1;
+    let contParcela2 = parcela2.length - 1;
+
+    let somatoria = 0, soma = 0;
+    let casa = 1;
+
+
+    while (contParcela1 >= 0 && contParcela2 >= 0) {
+        somatoria = parcela1[contParcela1] + parcela2[contParcela2];
+        soma += (somatoria * casa)
+        casa *= 10;
+        contParcela1--;
+        contParcela2--;
+    }
+    while (contParcela1 >= 0) {
+        soma += parcela1[contParcela1] * casa;
+        casa *= 10
+        contParcela1--;
+    } while (contParcela2 >= 0) {
+        soma += parcela2[contParcela2] * casa;
+        casa *= 10
+        contParcela2--;
+    }
+    return soma;
+}
+const numParaArray = function (valor) {
+    return valor.toString(10).split('').map(Number)
+}
+
+console.log(calcAdicao(40, 41))
+
 // Multiplicação Americana
 const calcMultAmerican = function (multiplicador, passo) {
     let i = unidMultiplicando.length - 1;
@@ -54,5 +90,4 @@ const calcMultRusso = function (valor1, valor2) {
     }
     return console.log(imparesCalc);
 }
-
-calcMultRusso(valor1, valor2);
+calcMultRusso(37, 550);
